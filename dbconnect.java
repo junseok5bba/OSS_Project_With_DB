62a091e55f401b044373d7c0f4f79235a649b80d
mvlfQuad <49227972+mvlfQuad@users.noreply.github.com>
2019-05-29 00:51:25 +0900

    revise code
    line16~17 edit.

--- dbconnect.java
+++ dbconnect.java
@@ -13,8 +13,7 @@ public class dbconnect {
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			// MySQL 서버를 설정합니다.
-			con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:436/vocabulary?serverTimezone=Asia/Seoul", "root",
-					"oss206");
+			con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:436/vocabulary?serverTimezone=Asia/Seoul","root","oss206");
 			st = con.createStatement();
 		} catch (Exception ex) {
 			System.out.println("Exception :" + ex);
