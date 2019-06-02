4cb46fb9f2346059d62f9fc603c99f8f3b15d07c
junseok5bba <49227938+junseok5bba@users.noreply.github.com>
2019-06-02 22:13:56 +0900

    Update Main.java
    create table(새 단어장, 오답노트) 및 불러오기(수능,토익,토플,새 단어장, 오답노트)
    단어삭제, 종료

--- Main.java
+++ Main.java
@@ -1,3 +1,5 @@
+package kr.ac.sunmoon;
+
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
 import java.sql.*;
@@ -9,141 +11,306 @@ import javax.swing.JTable;
 import javax.swing.JScrollPane;
 import javax.swing.JMenuItem;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.awt.event.ActionEvent;
 import javax.swing.JMenuBar;
 import javax.swing.JMenu;
+import javax.swing.JTextField;
+import javax.swing.*;
 import java.awt.Font;
+import java.awt.Frame;
+import java.awt.SystemColor;
+import java.awt.Color;
+import java.awt.Dimension;
 
 public class Main extends JFrame {
 
-   private JPanel contentPane;
-   private JTable table;
+	private JPanel contentPane;
+	private JTable table;
 
-   /**
-    * Launch the application.
-    */
-   public static void main(String[] args) {
-      EventQueue.invokeLater(new Runnable() {
-         public void run() {
-            try {
-               Main frame = new Main();
-               frame.setVisible(true);
-            } catch (Exception e) {
-               e.printStackTrace();
-            }
-         }
-      });
-   }
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Main frame = new Main();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
 
-   /**
-    * Create the frame.
-    */
-   Connection conn = null;
+	/**
+	 * Create the frame.
+	 */
+	Connection conn = null;
+	PreparedStatement pstmt = null;
+	String tablename;
+	String word_id;
+	String kor;
+	String eng;
+	String voca_id;
 
-   
-   public Main() {
-      table = new JTable();
-      setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      setBounds(100, 100, 789, 747);
-      contentPane = 
-    		  new JPanel();
-      contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-      setContentPane(contentPane);
-      contentPane.setLayout(null);
-      
-      JScrollPane scrollPane = new JScrollPane();
-      scrollPane.setBounds(31, 46, 355, 604);
-      contentPane.add(scrollPane);
-      scrollPane.setViewportView(table);
-      
-      JMenuBar menuBar = new JMenuBar();
-      menuBar.setBounds(0, 0, 765, 35);
-      contentPane.add(menuBar);
-      
-      JMenu file_menu = new JMenu("파일");
-      file_menu.setFont(new Font("맑은 고딕", Font.PLAIN, 21));
-      menuBar.add(file_menu);
-      
-      JMenuItem new_voca = new JMenuItem("새 단어장");
-      new_voca.addActionListener(new ActionListener() {
-      	public void actionPerformed(ActionEvent arg0) {
-      	}
-      });
-      file_menu.add(new_voca);
-      
-      JMenuItem view_myword = new JMenuItem("불러오기");
-      view_myword.addActionListener(new ActionListener() {
-      	public void actionPerformed(ActionEvent e) {
-      	}
-      });
-      file_menu.add(view_myword);
-      
-      JMenu odap_menu = new JMenu("오답노트");
-      odap_menu.setFont(new Font("맑은 고딕", Font.PLAIN, 21));
-      menuBar.add(odap_menu);
-      
-      JMenuItem new_odap = new JMenuItem("새 오답노트");
-      new_odap.addActionListener(new ActionListener() {
-      	public void actionPerformed(ActionEvent e) {
-      	}
-      });
-      odap_menu.add(new_odap);
-      
-      JMenuItem view_odap = new JMenuItem("불러오기");
-      view_odap.addActionListener(new ActionListener() {
-      	public void actionPerformed(ActionEvent e) {
-      	}
-      });
-      odap_menu.add(view_odap);
-      
-      JMenuItem suneung = new JMenuItem("수능");
-      suneung.setFont(new Font("맑은 고딕", Font.PLAIN, 18));
-      menuBar.add(suneung);
-      JMenuItem toeic = new JMenuItem("토익");
-      toeic.setFont(new Font("맑은 고딕", Font.PLAIN, 18));
-      menuBar.add(toeic);
-      
-      JMenuItem tople = new JMenuItem("토플");
-      tople.setFont(new Font("맑은 고딕", Font.PLAIN, 18));
-      menuBar.add(tople);
-      toeic.addActionListener(new ActionListener() {
-      	public void actionPerformed(ActionEvent e) {
-      		String sql = "select 영단어, 해석 from toeic";
-            try {
-               conn = dbconnect.dbconn();
-               Statement st = conn.createStatement();
-               ResultSet rs = st.executeQuery(sql);
-               table.setModel(DbUtils.resultSetToTableModel(rs));
-            } catch (Exception ex) {
-               System.out.println(ex.getMessage());
-            }
-      	}
-      });
-      suneung.addActionListener(new ActionListener() {
-      	public void actionPerformed(ActionEvent arg0) {
-      		String sql = "select 영단어, 해석 from 테스트";
-            try {
-               conn = dbconnect.dbconn();
-               Statement st = conn.createStatement();
-               ResultSet rs = st.executeQuery(sql);
-               table.setModel(DbUtils.resultSetToTableModel(rs));
-            } catch (Exception e) {
-               System.out.println(e.getMessage());
-            }
-      	}
-      });
-      tople.addActionListener(new ActionListener() {
-        	public void actionPerformed(ActionEvent arg0) {
-        		String sql = "select 영단어, 해석 from word";
-              try {
-                 conn = dbconnect.dbconn();
-                 Statement st = conn.createStatement();
-                 ResultSet rs = st.executeQuery(sql);
-                 table.setModel(DbUtils.resultSetToTableModel(rs));
-              } catch (Exception e) {
-                 System.out.println(e.getMessage());
-              }             
-        	}
-        });
-   }
+	private JTextField wordeng;
+	private JTextField wordkor;
+
+	public Main() {
+		table = new JTable();
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 780, 747);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(31, 46, 355, 604);
+		contentPane.add(scrollPane);
+		scrollPane.setViewportView(table);
+
+		JMenuBar menuBar = new JMenuBar();
+		menuBar.setBounds(0, 0, 765, 35);
+		contentPane.add(menuBar);
+
+		JMenu file_menu = new JMenu("파일");
+		file_menu.setFont(new Font("맑은 고딕", Font.PLAIN, 21));
+		menuBar.add(file_menu);
+
+		JMenuItem new_voca = new JMenuItem("새 단어장");
+		new_voca.setFont(new Font("맑은 고딕", Font.PLAIN, 18));
+		new_voca.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				try {
+					conn = dbconnect.dbconn();
+					Statement st = conn.createStatement();
+					StringBuilder sb = new StringBuilder();
+					String sql = sb.append("create table 새_단어장(").append("wordid int(10) auto_increment, ")
+							.append("영단어 VARCHAR(50), ").append("해석 VARCHAR(50), ").append("vocaid int(10), ")
+							.append("primary key(wordid), ")
+							.append("constraint 새_단어장_fk_vocaid foreign key(vocaid) references voca(vocaid)")
+							.append(");").toString();
+					st.execute(sql);
+					tablename = "새_단어장";
+					sql = "select 영단어, 해석 from " + tablename;
+					ResultSet rs = st.executeQuery(sql);
+					table.setModel(DbUtils.resultSetToTableModel(rs));
+				} catch (Exception ex) {
+					System.out.println(ex.getMessage());
+				}
+			}
+		});
+		file_menu.add(new_voca);
+
+		JMenuItem view_myword = new JMenuItem("불러오기");
+		view_myword.setFont(new Font("맑은 고딕", Font.PLAIN, 18));
+		view_myword.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String sql = "select 영단어, 해석 from 새_단어장";
+				try {
+					conn = dbconnect.dbconn();
+					Statement st = conn.createStatement();
+					ResultSet rs = st.executeQuery(sql);
+					table.setModel(DbUtils.resultSetToTableModel(rs));
+					tablename = "새_단어장";
+					voca_id = "4";
+				} catch (Exception f) {
+					System.out.println(f.getMessage());
+				}
+			}
+		});
+		file_menu.add(view_myword);
+
+		JMenu odap_menu = new JMenu("오답노트");
+		odap_menu.setFont(new Font("맑은 고딕", Font.PLAIN, 21));
+		menuBar.add(odap_menu);
+
+		JMenuItem new_odap = new JMenuItem("새 오답노트");
+		new_odap.setFont(new Font("맑은 고딕", Font.PLAIN, 18));
+		new_odap.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				try {
+					conn = dbconnect.dbconn();
+					Statement st = conn.createStatement();
+					StringBuilder sb = new StringBuilder();
+					String sql = sb.append("create table 오답노트(").append("wordid int(10) auto_increment, ")
+							.append("영단어 VARCHAR(50), ").append("해석 VARCHAR(50), ").append("vocaid int(10), ")
+							.append("primary key(wordid), ")
+							.append("constraint 오답노트_fk_vocaid foreign key(vocaid) references voca(vocaid)")
+							.append(");").toString();
+					st.execute(sql);
+					tablename = "오답노트";
+					sql = "select 영단어, 해석 from " + tablename;
+					ResultSet rs = st.executeQuery(sql);
+					table.setModel(DbUtils.resultSetToTableModel(rs));
+				} catch (Exception e) {
+					System.out.println(e.getMessage());
+				}
+			}
+		});
+		odap_menu.add(new_odap);
+
+		JMenuItem view_odap = new JMenuItem("불러오기");
+		view_odap.setFont(new Font("맑은 고딕", Font.PLAIN, 18));
+		view_odap.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String sql = "select 영단어, 해석 from 오답노트";
+				try {
+					conn = dbconnect.dbconn();
+					Statement st = conn.createStatement();
+					ResultSet rs = st.executeQuery(sql);
+					table.setModel(DbUtils.resultSetToTableModel(rs));
+					tablename = "오답노트";
+					voca_id = "5";
+				} catch (Exception d) {
+					System.out.println(d.getMessage());
+				}
+			}
+		});
+		odap_menu.add(view_odap);
+
+		JMenuItem suneung = new JMenuItem("수능");
+		suneung.setFont(new Font("맑은 고딕", Font.PLAIN, 18));
+		suneung.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				String sql = "select 영단어, 해석 from 수능";
+				try {
+					conn = dbconnect.dbconn();
+					Statement st = conn.createStatement();
+					ResultSet rs = st.executeQuery(sql);
+					table.setModel(DbUtils.resultSetToTableModel(rs));
+					tablename = "수능";
+					voca_id = "1";
+				} catch (Exception a) {
+					System.out.println(a.getMessage());
+				}
+			}
+		});
+		menuBar.add(suneung);
+
+		JMenuItem toeic = new JMenuItem("토익");
+		toeic.setFont(new Font("맑은 고딕", Font.PLAIN, 18));
+		toeic.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String sql = "select 영단어, 해석 from toeic";
+				try {
+					conn = dbconnect.dbconn();
+					Statement st = conn.createStatement();
+					ResultSet rs = st.executeQuery(sql);
+					table.setModel(DbUtils.resultSetToTableModel(rs));
+					tablename = "toeic";
+					voca_id = "2";
+				} catch (Exception c) {
+					System.out.println(c.getMessage());
+				}
+			}
+		});
+		menuBar.add(toeic);
+
+		JMenuItem tople = new JMenuItem("토플");
+		tople.setFont(new Font("맑은 고딕", Font.PLAIN, 18));
+		tople.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				String sql = "select 영단어, 해석 from tople";
+				try {
+					conn = dbconnect.dbconn();
+					Statement st = conn.createStatement();
+					ResultSet rs = st.executeQuery(sql);
+					table.setModel(DbUtils.resultSetToTableModel(rs));
+					tablename = "tople";
+					voca_id = "3";
+				} catch (Exception b) {
+					System.out.println(b.getMessage());
+				}
+			}
+		});
+		menuBar.add(tople);
+
+		JPanel wordpanel = new JPanel();
+		wordpanel.setBackground(Color.WHITE);
+		wordpanel.setBounds(399, 46, 353, 149);
+		contentPane.add(wordpanel);
+		wordpanel.setLayout(null);
+
+		wordeng = new JTextField();
+		wordeng.setBounds(88, 26, 138, 31);
+		wordpanel.add(wordeng);
+		wordeng.setColumns(10);
+
+		wordkor = new JTextField();
+		wordkor.setBounds(88, 75, 138, 31);
+		wordpanel.add(wordkor);
+		wordkor.setColumns(10);
+
+		JButton addword = new JButton("추가");
+		addword.setBackground(SystemColor.control);
+		addword.setFont(new Font("맑은 고딕", Font.PLAIN, 21));
+		addword.setBounds(250, 19, 91, 37);
+		wordpanel.add(addword);
+
+		JButton delword = new JButton("삭제");
+		delword.setBackground(SystemColor.control);
+		delword.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				int row = table.getSelectedRow();
+				int col = 0;
+				Object value = table.getValueAt(row, col);
+				try {
+					Statement st = conn.createStatement();
+					StringBuilder sb = new StringBuilder();
+					String sql = sb.append("delete from " + tablename + " where 영단어 = '").append(value + "'")
+							.append(";").toString();
+					st.executeUpdate(sql);
+					sql = "select 영단어, 해석 from " + tablename;
+					ResultSet rs = st.executeQuery(sql);
+					table.setModel(DbUtils.resultSetToTableModel(rs));
+				} catch (SQLException del) {
+					// TODO Auto-generated catch block
+					del.printStackTrace();
+				}
+			}
+		});
+		delword.setFont(new Font("맑은 고딕", Font.PLAIN, 21));
+		delword.setBounds(250, 75, 91, 37);
+		wordpanel.add(delword);
+
+		JLabel wordeng_label = new JLabel("영단어");
+		wordeng_label.setFont(new Font("맑은 고딕", Font.PLAIN, 18));
+		wordeng_label.setBounds(19, 26, 61, 25);
+		wordpanel.add(wordeng_label);
+
+		JLabel wordkorlabel = new JLabel("해석");
+		wordkorlabel.setFont(new Font("맑은 고딕", Font.PLAIN, 18));
+		wordkorlabel.setBounds(18, 81, 43, 25);
+		wordpanel.add(wordkorlabel);
+
+		JPanel optionpanel = new JPanel();
+		optionpanel.setBackground(Color.WHITE);
+		optionpanel.setBounds(398, 528, 354, 122);
+		contentPane.add(optionpanel);
+		optionpanel.setLayout(null);
+
+		
+		
+		JButton popupbutton = new JButton("미니 모드");
+		popupbutton.setBackground(SystemColor.control);
+		popupbutton.setFont(new Font("맑은 고딕", Font.PLAIN, 18));
+		popupbutton.setBounds(12, 18, 161, 86);
+		optionpanel.add(popupbutton);
+		
+		JButton quitbutton = new JButton("종료");
+		quitbutton.setBackground(SystemColor.control);
+		quitbutton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});
+		quitbutton.setFont(new Font("맑은 고딕", Font.PLAIN, 18));
+		quitbutton.setBounds(190, 18, 152, 86);
+		optionpanel.add(quitbutton);
+	}
 }
