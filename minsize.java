c6b01fc192c65610760da10275ddb23ff91a3826
mvlfQuad <49227972+mvlfQuad@users.noreply.github.com>
2019-06-07 22:23:18 +0900

    update minsize
    for문안에서 여러번 업데이트가 안되는것을 Timer기능을 사용하여 수정함

--- minsize.java
+++ minsize.java
@@ -1,10 +1,17 @@
+package kr.ac.sunmoon;
+
 import java.awt.BorderLayout;
+import java.util.*;
+import java.util.Map.Entry;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.SwingConstants;
 import javax.swing.border.EmptyBorder;
+import javax.swing.text.SimpleAttributeSet;
+import javax.swing.text.StyleConstants;
+import javax.swing.text.StyledDocument;
 
 import net.proteanit.sql.*;
 
@@ -34,82 +41,105 @@ import java.awt.Color;
 
 public class minsize extends JFrame {
 
-	private JPanel contentPane;
+   private JPanel contentPane;
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					String tablename = null;
-					minsize frame = new minsize(tablename);
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+   /**
+    * Launch the application.
+    */
+   public static void main(String[] args) {
+      EventQueue.invokeLater(new Runnable() {
+         public void run() {
+            try {
+               String tablename = null;
+               minsize frame = new minsize(tablename);
+               frame.setVisible(true);
+            } catch (Exception e) {
+               e.printStackTrace();
+            }
+         }
+      });
+   }
 
-	/**
-	 * Create the frame.
-	 */
-	Connection conn = null;
-	Statement stmt = null;
-	ResultSet rs = null;
-	int i = 0;
+   /**
+    * Create the frame.
+    */
+   Connection conn = null;
+   Statement stmt = null;
+   ResultSet rs = null;
+   int i = 0;
 
-	public minsize(String tablename) {
+   public minsize(String tablename) {
+      setAlwaysOnTop(true);
+      setResizable(false);
+      setBounds(100, 100, 301, 160);
+      contentPane = new JPanel();
+      contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+      setContentPane(contentPane);
+                  contentPane.setLayout(null);
+            
+                  JLabel engl = new JLabel();
+                  engl.setHorizontalAlignment(SwingConstants.CENTER);
+                  engl.setBounds(0, 18, 289, 38);
+                  engl.setFont(new Font("굴림", Font.BOLD, 20));
+                  contentPane.add(engl);
 
-		setAlwaysOnTop(true);
-		setResizable(false);
-		setBounds(100, 100, 300, 160);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		contentPane.setLayout(new GridLayout(0, 2, 0, 0));
+                  SimpleAttributeSet ecenter = new SimpleAttributeSet();
+                  StyleConstants.setAlignment(ecenter, StyleConstants.ALIGN_CENTER);
 
-		JTextPane engl = new JTextPane();
-		contentPane.add(engl);
+                  
+                  
+            JLabel korl = new JLabel();
+            korl.setHorizontalAlignment(SwingConstants.CENTER);
+            korl.setBounds(0, 58, 289, 38);
+            korl.setFont(new Font("굴림", Font.BOLD, 20));
+            contentPane.add(korl);
 
-		JTextPane korl = new JTextPane();
-		contentPane.add(korl);
+            SimpleAttributeSet center = new SimpleAttributeSet();
+            StyleConstants.setAlignment(center, StyleConstants.ALIGN_CENTER);
 
-		addWindowListener(new WindowAdapter() {
-			@Override
-			public void windowActivated(WindowEvent e) {
-				try {
-					Thread myThread = new Thread();
-					conn = dbconnect.dbconn();
-					stmt = conn.createStatement();
-					String usevocabulary = "use vocabulary";
-					stmt.executeUpdate(usevocabulary);
 
-					String showword = "select * from " + tablename;
-					rs = stmt.executeQuery(showword);
+      ArrayList<String> eng = new ArrayList<>();
+      ArrayList<String> kor = new ArrayList<>();
 
-					ArrayList<String> eng = new ArrayList<>();
-					ArrayList<String> kor = new ArrayList<>();
+      addWindowListener(new WindowAdapter() {
+         @Override
+         public void windowActivated(WindowEvent e) {
+            try {
+               conn = dbconnect.dbconn();
+               stmt = conn.createStatement();
+               String usevocabulary = "use vocabulary";
+               stmt.executeUpdate(usevocabulary);
 
-					while (rs.next()) {
-						eng.add(rs.getString(2));
-						kor.add(rs.getString(3));
-					}
+               String showword = "select * from " + tablename;
+               rs = stmt.executeQuery(showword);
 
-					for (int x = 0; x < eng.size(); x++) {
-						System.out.println(eng.get(x));
-						System.out.println(kor.get(x));
+               while (rs.next()) {
+                  eng.add(rs.getString(2));
+                  kor.add(rs.getString(3));
+               }
 
-						engl.setText(eng.get(x));
-						korl.setText(kor.get(x));
-						myThread.sleep(3000);
-					}
-				} catch (Exception d) {
-					System.out.println(d.getMessage());
-				}
-			}
-		});
-	}
+               Timer timer;
+               TimerTask timerTask;
+
+               timer = new Timer();
+               timerTask = new TimerTask() {
+                  int x = 0;
+
+                  public void run() {
+                     engl.setText(eng.get(x));
+                     korl.setText(kor.get(x));
+                     x++;
+                     if (x >= eng.size())
+                        x = 0;
+                  }
+
+               };
+               timer.schedule(timerTask, 500, 3000);
+            } catch (Exception d) {
+               System.out.println(d.getMessage());
+            }
+         }
+      });
+   }
+
 }
